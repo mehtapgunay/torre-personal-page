3d5535559da661c45107cfb36ff45e440b7fa8df
Mehtap Gunay <mehtap.gunay@gmail.com>
2023-04-07 01:15:39 +0100

    updated for github page api call issue

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,32 +1,39 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, catchError, map, of } from 'rxjs';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError, map, of } from 'rxjs';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
 import { UserModel } from '../models/user-model';
+import { isDevMode } from '@angular/core';
 
-const apiUrl = '/api/bios/'
-const  testUserId = 'torre';
+const apiSuffix = '/api/bios/';
+const baseApiUrl = `https://bio.torre.co${apiSuffix}`;
+const apiUrl = isDevMode() ? apiSuffix : baseApiUrl;
+const testUserId = 'torre';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-  constructor(public http: HttpClient) { }
-
+  constructor(public http: HttpClient) {}
 
   getUserInfo(userId: string = testUserId) {
     // user id might come from any user list..
-    const url = apiUrl+userId;
+    const url = apiUrl + userId;
     return this.http.get<any>(url, { headers: this.getHeader() }).pipe(
-      map(user => {
+      map((user) => {
         // manage error issues
         if (user['errorMessage']) {
           this.handleError(`getUserInfo error, user id: ${testUserId}`);
         }
         console.log(user);
         return user;
-
       }),
-      catchError(err => this.handleError(`getUserInfo error, user id: ${testUserId} - ${err}`))
+      catchError((err) =>
+        this.handleError(`getUserInfo error, user id: ${testUserId} - ${err}`)
+      )
     );
   }
 
@@ -38,9 +45,8 @@ export class UserService {
   private getHeader() {
     return new HttpHeaders({
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Methods':'GET',
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Methods': 'GET',
+      'Access-Control-Allow-Origin': '*',
     });
   }
-
 }
